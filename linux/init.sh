1f5ec214f84322c0f746bf3b19c5c53d2a12c1d6
AoEiuV020 <aoeiuv020@qq.com>
2016-12-09 02:20:44 +0800

    备份的旧.bashrc不调用
    其实就是改了个没用的名字.bashrc.old，
    
    还改了邮箱，改成英文字母QQ邮箱，
    顺便删除了开头几行相当于作者信息的注释，

--- linux/init.sh
+++ linux/init.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
-#	***************************************************
-#		^> File Name: init.sh
-#		^> Author: AoEiuV020
-#		^> Mail: 490674483@qq.com
-#		^> Created Time: 2016/01/31 - 23:07:20
-#	***************************************************
 dir=$(dirname $0)
 cd ${dir}
 echo ${PWD}
 ln -sf ${PWD}/.vimrc ~/.vimrc
-if [ -f ~/.bashrc ]
-then
-	mv ~/.bashrc ~/local.bashrc
-fi
+#if [ -f ~/.bashrc ]
+#then
+#	mv ~/.bashrc ~/local.bashrc
+#fi
 ln -sf ${PWD}/.bashrc ~/.bashrc
-git config --global user.email "490674483@qq.com"
+git config --global user.email "aoeiuv020@qq.com"
 git config --global user.name "AoEiuV020"
 git config --global push.default simple
