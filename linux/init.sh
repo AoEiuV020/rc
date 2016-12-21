04075cfa99669849ff35f2f5ea3f94eb4131865d
AoEiuV020 <aoeiuv020@gmail.com>
2016-12-22 07:05:06 +0800

    邮箱改成gmail

--- linux/init.sh
+++ linux/init.sh
@@ -8,6 +8,6 @@ ln -sf ${PWD}/.vimrc ~/.vimrc
 #	mv ~/.bashrc ~/local.bashrc
 #fi
 ln -sf ${PWD}/.bashrc ~/.bashrc
-git config --global user.email "aoeiuv020@qq.com"
+git config --global user.email "aoeiuv020@gmail.com"
 git config --global user.name "AoEiuV020"
 git config --global push.default simple
