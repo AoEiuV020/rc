f0cd0425ecfb4ba7be464a957b4123540861a314
AoEiuV020 <aoeiuv020@gmail.com>
2017-01-20 03:40:13 +0800

    添加c#

--- windows/work/my.bat
+++ windows/work/my.bat
@@ -19,6 +19,7 @@ set PUTTY_HOME=D:\aoeiuv020\programe\putty
 set VIM_HOME=D:\aoeiuv020\programe\text\vim\vim80
 set ARIA_HOME=D:\aoeiuv020\programe\download\aria2
 set ARIA_DOWNLOAD=D:\aoeiuv020\download\aria2
+set VSTOOLS_HOME="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools"
 rem if "%1"=="" (goto add) 
 goto %1
 goto end
@@ -28,7 +29,7 @@ vim %~f0
 goto end
 
 :java
-echo 		Working with java...
+echo 		Working with java,
 set JAVA_HOME=%JAVA_HOME%\
 set lib=%JAVA_HOME%lib;%lib%
 set include=%JAVA_HOME%include;%include%
@@ -36,7 +37,7 @@ set path=%JAVA_HOME%bin;%path%
 goto end
 
 :mingw
-echo 		Working with MinGW...
+echo 		Working with MinGW,
 set lib=%MinGW_HOME%\lib;%lib%
 set include=%MinGW_HOME%\include;%include%
 set path=%MinGW_HOME%\bin;%path%
@@ -56,42 +57,42 @@ call %0 ndk
 goto end
 
 :adb
-echo 		Android Debug Bridge...
+echo 		Android Debug Bridge,
 set path=%ADB_HOME%;%path%
 goto end
 
 :ant
-echo 		Working with android ant...
+echo 		Working with android ant,
 set path=%ANT_HOME%\bin;%path%
 goto end
 
 :sdk
-echo 		Working with android sdk...
+echo 		Working with android sdk,
 set SDK_TOOLS=%SDK_HOME%\tools
 set SDK_BUILD_TOOLS=%SDK_HOME%\build-tools\19.1.0
 set path=%SDK_BUILD_TOOLS%;%SDK_TOOLS%;%path%
 goto end
 
 :ndk
-echo 		Working with android ndk...
+echo 		Working with android ndk,
 set path=%NDK_HOME%;%path%
 goto end
 
 :msys
-echo 		Working with msys...
+echo 		Working with msys,
 set path=%MSYS_HOME%;%path%
 msys2
 goto exit
 
 :putty
-echo 		SSH with putty...
+echo 		SSH with putty,
 echo 		putty -ssh aoeiuv020.top -l aoeiuv -pw aoeiuv
 echo		psftp aoeiuv@aoeiuv020.top -pw aoeiuv
 set path=%PUTTY_HOME%;%path%
 goto end
 
 :add
-echo 		add my.bat...
+echo 		add my.bat,
 set env=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
 set temp=
 set MYWORK_HOME=%~dp0
@@ -104,21 +105,26 @@ call %0 addreg
 goto end
 
 :addvim
-echo 		add vim...
+echo 		add vim,
 %VIM_HOME%/install.exe
 goto end
 
 :addreg
-echo 		add reg...
+echo 		add reg,
 regedit cmd.reg
 regedit gvim.reg
 regedit utc.reg
 goto end
 
 :aria
-echo 		aria2c...
+echo 		aria2c,
 set path=%ARIA_HOME%;%path%
 cd /d %ARIA_DOWNLOAD%
+goto end
+
+:cs
+echo		c# csharp,
+call %VSTOOLS_HOME%\VsDevCmd.bat
 goto end
 
 :exit
