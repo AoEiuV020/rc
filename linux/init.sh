8ef2f50fab406888cc7e68e9191d190d08b83874
AoEiuV020 <aoeiuv020@qq.com>
2017-03-01 12:10:07 +0800

    .bashrc是软链接则不备份
    顺便添加个设置editor为vim,

--- linux/init.sh
+++ linux/init.sh
@@ -3,11 +3,12 @@ dir=$(dirname $0)
 cd ${dir}
 echo ${PWD}
 ln -sf ${PWD}/.vimrc ~/.vimrc
-#if [ -f ~/.bashrc ]
-#then
-#	mv ~/.bashrc ~/local.bashrc
-#fi
+if [ -f ~/.bashrc ] && [ ! -h ~/.bashrc ]
+then
+	mv ~/.bashrc ~/.bashrc.old
+fi
 ln -sf ${PWD}/.bashrc ~/.bashrc
 git config --global user.email "aoeiuv020@gmail.com"
 git config --global user.name "AoEiuV020"
 git config --global push.default simple
+git config --global core.editor vim
