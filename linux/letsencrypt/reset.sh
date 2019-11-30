a59f981110d4e596e067526985536d5ceae95193
AoEiuV020 <aoeiuv020@gmail.com>
2019-12-01 00:06:23 +0800

    reset all domain

---
+++ linux/letsencrypt/reset.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+oldpwd=$PWD
+cd $(dirname $0)
+export keyScript=${1:-$PWD/key.sh}
+if test ! -e "$keyScript"; then
+  echo $keyScript not found
+  exit 3
+fi
+if test ! -r "$keyScript"; then
+  echo $keyScript can not read
+  exit 4
+fi
+
+# 删除记录，
+domainList=($(jq -r 'keys|.[]' domain.json))
+for domain in $domainList; do
+    dnsServer=$(jq -r ".\"$domain\"" domain.json)
+    $PWD/$dnsServer/reset.sh $domain
+done
