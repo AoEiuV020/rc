71eb2fa9864229bc306d6f53a46b07e70e0252f6
AoEiuV020 <aoeiuv020@gmail.com>
2019-11-30 20:23:51 +0800

    add tencent install script

--- linux/letsencrypt/tencent/install.sh
+++ linux/letsencrypt/tencent/install.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-# 需要安装pip, 
-# 依赖环境：Python 2.7 到 3.6 版本
-# ubuntu: sudo apt install -y python-pip
-set -e
-if test "0" -ne "$(id -u)"; then
-    echo require root
-    exit 1
-fi
-pip install tencentcloud-sdk-python
+# 依赖环境：openssl, curl
