3aa601b923844bc6341b11e19fda2f5e21c4b64d
AoEiuV020 <aoeiuv020@gmail.com>
2019-11-30 16:05:07 +0800

    add certbot renew hook script

---
+++ linux/letsencrypt/certbot-renew-hook.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+oldpwd=$PWD
+tmpdir=$(mktemp -u)
+mkdir $tmpdir
+cd $(dirname $0)
+domain=$CERTBOT_DOMAIN
+value=$CERTBOT_VALIDATION
+keyScript=${1:-$PWD/key.sh}
+if test -z "$domain"; then
+    echo domain empty
+    exit 1
+fi
+if test -z "$value"; then
+    echo value empty
+    exit 2
+fi
+if test ! -e "$keyScript"; then
+    echo $keyScript not found
+    exit 3
+fi
+if test ! -r "$keyScript"; then
+    echo $keyScript can not read
+    exit 4
+fi
+
+dnsServer=$(jq -r ".\"$domain\"" domain.json)
+if test ! -d "$dnsServer"; then
+    echo "$domain => $dnsServer not support"
+    exit 5
+fi
+echo $PWD/$dnsServer/add.sh $domain $value $keyScript
+$PWD/$dnsServer/add.sh $domain $value $keyScript
+
+cd $oldpwd
+rm -rf $tmpdir
