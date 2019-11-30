c6f21e50b1c6d8bdf0c7463d1971c2493d616a16
AoEiuV020 <aoeiuv020@gmail.com>
2019-11-30 14:52:48 +0800

    add aliyun cli add script

---
+++ linux/letsencrypt/aliyun/add.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+oldpwd=$PWD
+tmpdir=$(mktemp -u)
+mkdir -p $tmpdir
+cd $(dirname $0)
+domain=$1
+value=$2
+#dryrun='--dryrun'
+
+subDomain=_acme-challenge.$domain
+# 先删除之前设置的记录，
+idList=($(aliyun alidns DescribeSubDomainRecords --SubDomain $subDomain \
+    |jq .DomainRecords.Record \
+    |jq -r '.[].RecordId' \
+    ))
+for id in ${idList[*]}; do
+    echo aliyun alidns DeleteDomainRecord  --RecordId $id $dryrun
+    aliyun alidns DeleteDomainRecord  --RecordId $id $dryrun
+done
+
+# 添加记录，
+echo aliyun alidns AddDomainRecord --DomainName $domain --RR _acme-challenge --Type TXT --Value $value $dryrun
+aliyun alidns AddDomainRecord --DomainName $domain --RR _acme-challenge --Type TXT --Value $value $dryrun
+
+cd $oldpwd
+rm -rf $tmpdir
