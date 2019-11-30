8f86c7a196106fc7dd53794e4833519975203309
AoEiuV020 <aoeiuv020@gmail.com>
2019-11-30 23:33:11 +0800

    remove useless variable

--- linux/letsencrypt/aliyun/reset.sh
+++ linux/letsencrypt/aliyun/reset.sh
@@ -3,16 +3,11 @@ set -e
 oldpwd=$PWD
 cd $(dirname $0)
 domain=$1
-value=$2
 #dryrun='--dryrun'
 
 if test -z "$domain"; then
   echo domain empty
   exit 1
-fi
-if test -z "$value"; then
-  echo value empty
-  exit 2
 fi
 
 subDomain=_acme-challenge
