d6764685a091741ac0e1e3aa468dcca82ea54ecc
AoEiuV020 <aoeiuv020@gmail.com>
2019-11-30 18:51:39 +0800

    add tencent query record script

---
+++ linux/letsencrypt/tencent/get.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+oldpwd=$PWD
+cd $(dirname $0)
+domain=$1
+if test -z "$domain"; then
+    echo domain empty
+    exit 1
+fi
+keyScript=${2:-$PWD/../key.sh}
+. $keyScript
+
+tmpdir=$(mktemp -u)
+mkdir $tmpdir
+trap "{ rm -rf $tmpdir; }" EXIT
+cd $tmpdir
+
+echo key=$TENCENT_KEY >&2
+valueStr=$(echo {} \
+    |jq ".+{SecretId:\"$TENCENT_ID\"}" \
+    |jq ".+{Region:\"ap-guangzhou\"}" \
+    |jq ".+{Timestamp:\"$(date +%s)\"}" \
+    |jq ".+{Nonce:\"$(shuf -i 0-99999 -n 1)\"}" \
+    |jq ".+{SignatureMethod:\"HmacSHA256\"}" \
+    |jq ".+{Action:\"RecordList\"}" \
+    |jq ".+{offset:\"0\"}" \
+    |jq ".+{length:\"20\"}" \
+    |jq ".+{domain:\"$domain\"}" \
+    |jq ".+{subDomain:\"_acme-challenge\"}" \
+    |jq -S . \
+    |jq -r 'to_entries|map("\(.key)=\(.value)")|join("&")')
+apiDomain="cns.api.qcloud.com"
+srcStr="GET$apiDomain/v2/index.php?$valueStr"
+echo srcStr=$srcStr >&2
+
+sign=$(echo -n $srcStr|openssl sha256 -hmac $TENCENT_KEY -binary|base64)
+echo sign=$sign >&2
+
+url="https://$apiDomain/v2/index.php?$valueStr&Signature=$sign"
+echo curl $url >&2
+curl -s $url
+
