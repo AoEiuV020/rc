4614b751e17aeebf0ccef4c976a1af52352f8613
AoEiuV020 <aoeiuv020@gmail.com>
2020-07-13 23:40:16 +0800

    调整clash配置
    服务器使用clash.service,
    配置文件放在/etc/clash,
    桌面使用clash-user.service,
    配置文件放在默认~/.config/clash,
    桌面自启动使用clash.desktop,
    其他不能在桌面启动后自启设置系统代理，
    更新订阅使用update.sh，
    更新到当前目录config.yaml,

--- linux/clash/system-proxy.sh
+++ linux/clash/system-proxy.sh
@@ -4,10 +4,6 @@ export http_proxy_host=${http_proxy_host:=127.0.0.1}
 export socks_proxy_host=${socks_proxy_host:=127.0.0.1}
 export http_proxy_port=${http_proxy_port:=1082}
 export socks_proxy_port=${socks_proxy_port:=1081}
-HOME=$(eval echo ~$USER)
-export DISPLAY=:0
-export XDG_RUNTIME_DIR="/run/user/$UID"
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$UID/bus"
 dconf write /system/proxy/http/host "'$http_proxy_host'"
 dconf write /system/proxy/http/port "$http_proxy_port"
 dconf write /system/proxy/https/host "'$http_proxy_host'"
---
+++ linux/clash/update.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+echo update clash subscribe...
+profile=$(dirname $0)
+if [ ! -w $profile ]
+then
+    echo permission denied: $profile
+    exit
+fi
+TIME=`date '+%Y-%m-%dT%H:%M:%S'`
+BAK="config.yaml.bak${TIME}"
+DEFAULT=config.yaml
+export user=${user:=aoeiuv}
+export http_proxy_host=${http_proxy_host:=127.0.0.1}
+export socks_proxy_host=${socks_proxy_host:=127.0.0.1}
+export http_proxy_port=${http_proxy_port:=1082}
+export socks_proxy_port=${socks_proxy_port:=1081}
+
+cd $profile
+url=$1
+if [ "x$url" == "x" ]
+then
+    echo no subscribe url argument...
+    if [ -r subscribe ]
+    then
+        url=$(cat subscribe)
+    fi
+fi
+if [ "x$url" == "x" ]
+then
+    echo no subscribe url file...
+    exit
+fi
+echo updating $url
+curl --noproxy "*" -L -o $BAK $url
+if [ "$?" != 0 ]
+then
+    echo download failed...
+    exit
+fi
+sed -is '/^\S*port:/d' $BAK
+sed -is '/^allow-lan:/d' $BAK
+ex $BAK <<EOF
+1 i
+mixed-port: ${socks_proxy_port}
+port: ${http_proxy_port}
+socks-port: 11181
+allow-lan: false
+.
+wq
+EOF
+diff $DEFAULT $BAK &>/dev/null
+if [ "$?" != 0 ]
+then
+    echo updated...
+    mv $BAK $DEFAULT
+else
+    echo no update...
+    rm $BAK
+fi
