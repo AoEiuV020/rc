a612932339e968f87b85770fde6f6845d00372a1
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-26 22:57:27 +0800

    调整命令行代理设置变量

--- linux/clash/system-proxy.sh
+++ linux/clash/system-proxy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo setup system proxy...
-export http_proxy_host=${http_proxy_host:=127.0.0.1}
-export socks_proxy_host=${socks_proxy_host:=127.0.0.1}
-export http_proxy_port=${http_proxy_port:=1082}
-export socks_proxy_port=${socks_proxy_port:=1081}
+export ALL_PROXY="socks://${socks_proxy_host:=127.0.0.1}:${socks_proxy_port:=1081}/"
+export FTP_PROXY="http://${http_proxy_host:=127.0.0.1}:${http_proxy_port:=1082}/"
+export HTTPS_PROXY="http://${http_proxy_host:=127.0.0.1}:${http_proxy_port:=1082}/"
+export HTTP_PROXY="http://${http_proxy_host:=127.0.0.1}:${http_proxy_port:=1082}/"
+export all_proxy="socks://${socks_proxy_host:=127.0.0.1}:${socks_proxy_port:=1081}/"
+export ftp_proxy="http://${http_proxy_host:=127.0.0.1}:${http_proxy_port:=1082}/"
+export http_proxy="http://${http_proxy_host:=127.0.0.1}:${http_proxy_port:=1082}/"
+export https_proxy="http://${http_proxy_host:=127.0.0.1}:${http_proxy_port:=1082}/"
+
 dconf write /system/proxy/http/host "'$http_proxy_host'"
 dconf write /system/proxy/http/port "$http_proxy_port"
 dconf write /system/proxy/https/host "'$http_proxy_host'"
