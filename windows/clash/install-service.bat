6b55dda08fdf375812ce6be5973101c19b00b327
AoEiuV020 <aoeiuv020@gmail.com>
2021-03-09 09:50:48 +0800

    以管理员身份运行nssm配置服务
    记得以前用这脚本不需要特地申请管理员的，nssm自己会在必须的时候申请管理员权限，
    但现在nssm set会拒绝访问，可能是360干了什么，总之给管理员权限就正常了，

--- windows/clash/install-service.bat
+++ windows/clash/install-service.bat
@@ -18,12 +18,14 @@ goto :exit
 ) else (
 echo %exe% > %pwd%path
 )
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 for /f "eol=/ delims=" %%i in ('whoami') do set user=%%i
 echo 输入当前用户 %user% 的密码以便自动设置系统代理，
 set /p password=
 set nssm=%pwd%..\nssm\nssm.exe
 set start=%pwd%clash-service.bat
 set log=%temp%\clash-log
+# TODO: 加个判断，已经有了就不重复创建，
 %nssm% install clash %start%
 %nssm% set clash AppStdin nul
 %nssm% set clash AppStdout %log%
