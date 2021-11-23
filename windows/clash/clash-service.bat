87f28c450d6226202f3222f51790adc222db6334
AoEiuV020 <aoeiuv020@gmail.com>
2021-11-23 15:12:24 +0800

    系统代理不跳过私网ip

--- windows/clash/clash-service.bat
+++ windows/clash/clash-service.bat
@@ -14,7 +14,7 @@ exit /b 2
 )
 set sysproxy=%pwd%../sysproxy/sysproxy.exe
 echo 设置ie代理，
-%sysproxy% global 127.0.0.1:1081 "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*;<local>"
+%sysproxy% global 127.0.0.1:1081 "<local>"
 echo 启动clash, %TIME%
 %exe%
 echo 已结束clash, %TIME%
